84213567785f82bbdeea3c3755651be007e355f0
Mike <m.klimek@tu-bs.de>
2015-04-24 21:49:12 +0200

    Retry a request with the correct service url on ConnectException and prevent network requests when not in valid network

--- app/src/main/java/de/hundebarf/fundus/FundusApplication.java
+++ app/src/main/java/de/hundebarf/fundus/FundusApplication.java
@@ -7,10 +7,16 @@ import android.util.Log;
 
 import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.Credentials;
+import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.net.ConnectException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import de.hundebarf.fundus.collection.FundusAccount;
@@ -19,113 +25,99 @@ import de.hundebarf.fundus.database.ServiceHelper;
 import retrofit.Endpoint;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.android.MainThreadExecutor;
 import retrofit.client.OkClient;
 
 public class FundusApplication extends Application {
-	private static final String TAG = FundusApplication.class.getSimpleName();
+    private static final String TAG = FundusApplication.class.getSimpleName();
 
-	private SharedPreferences mPreferences;
-	private static final String FUNDUS_PREFERENCES = "FUNDUS_PREFERENCES";
-	private static final String USER_PREFERENCE = "USER_PREFERENCE";
-	private static final String PASSWORD_PREFERENCE = "PASSWORD_PREFERENCE";
-	private static final String SERVICE_URL_PREFERENCE = "SERVICE_URL_PREFERENCE";
+    private SharedPreferences mPreferences;
+    private static final String FUNDUS_PREFERENCES = "FUNDUS_PREFERENCES";
+    private static final String USER_PREFERENCE = "USER_PREFERENCE";
+    private static final String PASSWORD_PREFERENCE = "PASSWORD_PREFERENCE";
+    private static final String SERVICE_URL_PREFERENCE = "SERVICE_URL_PREFERENCE";
 
-	private ServiceConnection mServiceConnection;
+    private ServiceConnection mServiceConnection;
     private ServiceConnection mCacheConnection;
     private ServiceHelper mServiceHelper;
     private static final long TIMEOUT = 7; // seconds
 
-    private String mServiceURL = "http://192.168.42.202";
+    private String mServiceURL = "http://192.168.178.8";
     private FundusAccount mAccount;
 
     @Override
     public void onCreate() {
         mPreferences = getSharedPreferences(FUNDUS_PREFERENCES, MODE_PRIVATE);
-		
-		// Service URL
-		mServiceURL = mPreferences.getString(FundusApplication.SERVICE_URL_PREFERENCE, mServiceURL);
-		
-		// Valid SSIDs
-		String[] validSSIDs = getResources().getStringArray(R.array.valid_ssids);
-		mServiceHelper = new ServiceHelper(this, Arrays.asList(validSSIDs));
-		
-		// Account
-		String user = mPreferences.getString(USER_PREFERENCE, "");
-		String password = mPreferences.getString(PASSWORD_PREFERENCE, "");
-		mAccount = new FundusAccount(user, password);
-		super.onCreate();
-	}
 
-	public FundusAccount getAccount() {
-		return mAccount;
-	}
+        //Last known Service URL
+        mServiceURL = mPreferences.getString(FundusApplication.SERVICE_URL_PREFERENCE, mServiceURL);
+        Log.d(TAG, "Last Service URL: " + mServiceURL);
 
-	public void setAccount(FundusAccount account) {
-		Editor editor = mPreferences.edit();
-		if(account == null) {
-			editor.remove(USER_PREFERENCE);
-			editor.remove(PASSWORD_PREFERENCE);
-			mAccount = new FundusAccount("","");
-		} else {
-			editor.putString(USER_PREFERENCE, account.getUser());
-			editor.putString(PASSWORD_PREFERENCE, account.getPassword());
-			mAccount = account;
-		}
-		editor.apply();
-	}
+        // Valid SSIDs
+        String[] validSSIDs = getResources().getStringArray(R.array.valid_ssids);
+        mServiceHelper = new ServiceHelper(this, Arrays.asList(validSSIDs));
 
-	public String getServiceURL() {
-		return mServiceURL;
-	}
+        createCacheAndNetworkConnections();
 
-	public void setServiceURL(String serviceURL) {
-		mServiceURL = serviceURL;
-		mPreferences.edit().putString(SERVICE_URL_PREFERENCE, serviceURL).apply();
-	}
+        // Account
+        String user = mPreferences.getString(USER_PREFERENCE, "");
+        String password = mPreferences.getString(PASSWORD_PREFERENCE, "");
+        mAccount = new FundusAccount(user, password);
+        super.onCreate();
+    }
+
+    public FundusAccount getAccount() {
+        return mAccount;
+    }
+
+    public void setAccount(FundusAccount account) {
+        Editor editor = mPreferences.edit();
+        if (account == null) {
+            editor.remove(USER_PREFERENCE);
+            editor.remove(PASSWORD_PREFERENCE);
+            mAccount = new FundusAccount("", "");
+        } else {
+            editor.putString(USER_PREFERENCE, account.getUser());
+            editor.putString(PASSWORD_PREFERENCE, account.getPassword());
+            mAccount = account;
+        }
+        editor.apply();
+    }
 
     public ServiceConnection getServiceConnection() {
-        if (mServiceConnection == null) {
-            // lazy init
-            createCacheAndNetworkConnections();
-        }
         return mServiceConnection;
     }
 
     public ServiceConnection getCacheConnection() {
-        if (mCacheConnection == null) {
-            // lazy init
-            createCacheAndNetworkConnections();
-        }
         return mCacheConnection;
     }
 
     private void createCacheAndNetworkConnections() {
-        // Client for caching and timeouts
         OkHttpClient okHttpClient = new OkHttpClient();
-        try {
-			Cache cache = new Cache(getCacheDir(), 10 * 1024 * 1024);
-			okHttpClient.setCache(cache);
-		} catch (IOException e) {
-			Log.w(TAG, "Could not create httpClientCache", e);
-		}
         okHttpClient.setReadTimeout(TIMEOUT, TimeUnit.SECONDS);
         okHttpClient.setConnectTimeout(TIMEOUT, TimeUnit.SECONDS);
+        try {
+            Cache cache = new Cache(getCacheDir(), 10 * 1024 * 1024);
+            okHttpClient.setCache(cache);
+        } catch (IOException e) {
+            Log.w(TAG, "Could not create httpClientCache", e);
+        }
 
-        // forces cache
+        okHttpClient.interceptors().add(new CustomInterceptor());
+
+        // forces cache. used for cache connection
         // TODO fill complete cache at some point
-        RequestInterceptor cacheInterceptor = new FundusInterceptor() {
+        RequestInterceptor cacheInterceptor = new RequestInterceptor() {
             @Override
             public void intercept(RequestFacade request) {
-                super.intercept(request);
                 request.addHeader("Cache-Control", "only-if-cached, max-stale=" + Integer.MAX_VALUE);
             }
         };
 
-        // skips cache and forces full refresh
-        RequestInterceptor noCacheInterceptor = new FundusInterceptor() {
+        // skips cache and forces full refresh. used for service connection
+        RequestInterceptor noCacheInterceptor = new RequestInterceptor() {
             @Override
             public void intercept(RequestFacade request) {
-                super.intercept(request);
                 request.addHeader("Cache-Control", "no-cache");
             }
         };
@@ -134,20 +126,22 @@ public class FundusApplication extends Application {
         Endpoint endpoint = new Endpoint() {
             @Override
             public String getUrl() {
-				return mServiceURL;
-			}
-			@Override
-			public String getName() {
-				return "Fundus-Service";
-			}
-		};
+                return mServiceURL;
+            }
 
-		// Build RestAdapter
+            @Override
+            public String getName() {
+                return "Fundus-Service";
+            }
+        };
+
+        // Build RestAdapter
         RestAdapter.Builder builder = new RestAdapter.Builder()
                 .setEndpoint(endpoint)
-//				.setLogLevel(RestAdapter.LogLevel.FULL)
-				.setClient(new OkClient(okHttpClient))
-                .setRequestInterceptor(noCacheInterceptor);
+                .setClient(new OkClient(okHttpClient))
+                .setRequestInterceptor(noCacheInterceptor)
+                        // process requests sequentially
+                .setExecutors(Executors.newSingleThreadExecutor(), new MainThreadExecutor());
         mServiceConnection = builder.build().create(ServiceConnection.class);
 
         builder.setRequestInterceptor(cacheInterceptor);
@@ -155,20 +149,61 @@ public class FundusApplication extends Application {
     }
 
     /**
-     * Adds Authorization header (and User-Agent, etc.) to every request.
+     * Intercept each request and add headers, prevent network requests when not in valid network
+     * and retry a request with the correct service url on ConnectException
      */
-    private class FundusInterceptor implements RequestInterceptor {
+    private class CustomInterceptor implements Interceptor {
+        private static final String TAG = "Interceptor";
 
         @Override
-        public void intercept(RequestFacade request) {
-            request.addHeader("User-Agent", "Fundus-App");
-            request.addHeader("Accept", "application/json");
+        public Response intercept(Chain chain) throws IOException {
 
-            // basic access authentication
+            // add basic access authentication and other common headers to each request
             FundusAccount account = getAccount();
             String credentials = Credentials.basic(account.getUser(), account.getPassword());
-            request.addHeader("Authorization", credentials);
+            Request authorizedRequest = chain.request().newBuilder()
+                    .header("User-Agent", "Fundus-App")
+                    .header("Accept", "application/json")
+                    .header("Authorization", credentials)
+                    .build();
+
+            boolean useCache = authorizedRequest.cacheControl().onlyIfCached();
+            if (useCache) {
+                // everything okay, no need to check valid network etc.
+                return chain.proceed(authorizedRequest);
+            }
+
+            if (!mServiceHelper.inValidNetwork()) {
+                Log.v(TAG, "Not in valid network");
+                // abort request when not in valid wifi network
+                return null;
+            }
+
+            Response response;
+            try {
+                response = chain.proceed(authorizedRequest);
+            } catch (ConnectException e) {
+                // not a valid service url -> search service url and retry
+                String foundUrl = mServiceHelper.findServiceURL(mServiceURL);
+                if (foundUrl == null) {
+                    // service not found
+                    Log.v(TAG, "Could not find service");
+                    throw e;
+                }
+
+                // save new url
+                mServiceURL = foundUrl;
+                mPreferences.edit().putString(SERVICE_URL_PREFERENCE, foundUrl).apply();
+
+                // retry with new url
+                Log.v(TAG, "retry");
+                URL retryUrl = new URL(mServiceURL + authorizedRequest.url().getPath());
+                Request newUrlRequest = authorizedRequest.newBuilder().url(retryUrl).build();
+                return chain.proceed(newUrlRequest);
+            }
+
+            // service found
+            return response;
         }
     }
-
 }
--- app/src/main/java/de/hundebarf/fundus/database/ServiceHelper.java
+++ app/src/main/java/de/hundebarf/fundus/database/ServiceHelper.java
@@ -2,6 +2,8 @@ package de.hundebarf.fundus.database;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
@@ -28,172 +30,182 @@ import de.hundebarf.fundus.FundusApplication;
 import de.hundebarf.fundus.collection.FundusAccount;
 
 public class ServiceHelper {
-	private static final String TAG = ServiceHelper.class.getSimpleName();
+    private static final String TAG = ServiceHelper.class.getSimpleName();
 
-	private List<String> mValidSSIDs;
-	private FundusApplication mApp;
+    private List<String> mValidSSIDs;
+    private FundusApplication mApp;
 
-	private OkHttpClient mClient;
+    private OkHttpClient mClient;
 
-	public ServiceHelper(Context context, List<String> validSSIDs) {
-		mApp = (FundusApplication) context;
-		mValidSSIDs = validSSIDs;
-	}
-	
-//	public boolean isWifiEnabled() {
-//		ConnectivityManager connManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-//		NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-//		return wifi.isConnected();
-//	}
+    public ServiceHelper(FundusApplication context, List<String> validSSIDs) {
+        mApp = context;
+        mValidSSIDs = validSSIDs;
+    }
 
-	public boolean isSSIDValid() {
-		String ssid = getSSID();
-		return mValidSSIDs.contains(ssid);
-	}
+    public String findServiceURL(String lastKnownServiceURL) {
+        if (!inValidNetwork()) {
+            Log.i(TAG, "Not in valid Wifi Network");
+            return null;
+        }
 
-	public String getServiceURL() {
-		if(/*!isWifiEnabled() ||*/ !isSSIDValid()) {
-			Log.i(TAG, "Not in valid Wifi Network");
-			return null;
-		}
-		
-		String lastKnownUrl = mApp.getServiceURL();
-		if(isServiceUrlValid(lastKnownUrl)) {
-			Log.i(TAG, "Last known url is valid");
-			return lastKnownUrl;
-		}
-		
-		return discoverServiceURL();
-	}
+        if (isServiceUrlValid(lastKnownServiceURL)) {
+            Log.i(TAG, "Last known url is valid");
+            return lastKnownServiceURL;
+        }
 
-	private String getSSID() {
-		WifiManager wifiManager = (WifiManager) mApp.getSystemService(Context.WIFI_SERVICE);
-		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-		String ssid = wifiInfo.getSSID();
-		// remove quotation marks
-		return ssid.replace("\"", "");
-	}
-	
-	@SuppressLint("DefaultLocale")
-	private String getLocalIp() {
-		WifiManager wifiManager = (WifiManager) mApp.getSystemService(Context.WIFI_SERVICE);
-		int ip = wifiManager.getConnectionInfo().getIpAddress();
-		return String.format("%d.%d.%d.%d", (ip & 0xff), (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
-	}
+        return discoverServiceURL();
+    }
 
-	private OkHttpClient getClient() {
-		if (mClient != null) {
-			return mClient;
-		}
-		mClient = new OkHttpClient();
-		
-		FundusAccount account = mApp.getAccount();
-		final String credentials = Credentials.basic(account.getUser(), account.getPassword());
-		
-		Interceptor interceptor = new Interceptor() {
-			@Override
-			public com.squareup.okhttp.Response intercept(Chain chain) throws IOException {
-		        Request request = chain.request();
-		        Request modifiedRequest = request.newBuilder()
-		            .removeHeader("User-Agent")
-				    .addHeader("User-Agent", "Fundus-App")
-				    .addHeader("Accept", "application/json")
-				    .addHeader("Authorization", credentials)
-		            .build();
-		        return chain.proceed(modifiedRequest);
-			}
-		};
-		mClient.interceptors().add(interceptor);
-		
-		mClient.setReadTimeout(5, TimeUnit.SECONDS);
-		mClient.setConnectTimeout(5, TimeUnit.SECONDS);
-		return mClient;
-	}
+    public boolean inValidNetwork() {
+        return isWifiEnabled() && isSSIDValid();
+    }
 
-	private boolean isServiceUrlValid(String url) {
-		String responseURL = new CheckConnection(url, getClient()).call();
-		return (responseURL != null);
-	}
+    private boolean isWifiEnabled() {
+        ConnectivityManager connManager = (ConnectivityManager) mApp.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return wifi.isConnected();
+    }
 
-	private String discoverServiceURL() {
-		String ipAddress = getLocalIp();
-		ipAddress = ipAddress.substring(0, ipAddress.lastIndexOf("."));
-		ipAddress += ".";
+    private boolean isSSIDValid() {
+        String ssid = getSSID();
+        return mValidSSIDs.contains(ssid);
+    }
 
-		// executor for parallel requests
-		int range = 255;
-		int nrThreads = 255; // FIXME 255 threads maybe too much
-		ExecutorService executor = Executors.newFixedThreadPool(nrThreads);
-		CompletionService<String> complService = new ExecutorCompletionService<>(executor);
-		List<Future<String>> futures = new ArrayList<>(range);
-		
-		// free memory for threads
-		System.runFinalization();
-		System.gc();
+    private String getSSID() {
+        WifiManager wifiManager = (WifiManager) mApp.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String ssid = wifiInfo.getSSID();
+        // remove quotation marks
+        return ssid.replace("\"", "");
+    }
 
-		// create callables
-		for (int i = 0; i < range; i++) {
-			String curURL = "http://" + ipAddress + i + "/" + ServiceConnection.SERVICE_URI + "/";
-			CheckConnection checkConnection = new CheckConnection(curURL, getClient());
-			futures.add(complService.submit(checkConnection));
-		}
-		
-		// get results in completion order
-		for (int i = 0; i < range; i++) {
-			String possibleResult;
-			try {
-				possibleResult = complService.take().get();
-			} catch (ExecutionException e) {
-				// Ignore
-				continue;
-			} catch (InterruptedException e) {
-				// Ignore
-				continue;
-			}
-			if (possibleResult != null) {
-				// service found -> cancel other futures
-				for (Future<String> curFuture : futures) {
-					if (!curFuture.isDone() && !curFuture.isCancelled()) {
-						curFuture.cancel(false);
-					}
-				}
-				Log.i(TAG, "Found WebService at: " + possibleResult);
-				return possibleResult;
-			}
-		}
+    @SuppressLint("DefaultLocale")
+    private String getLocalIp() {
+        WifiManager wifiManager = (WifiManager) mApp.getSystemService(Context.WIFI_SERVICE);
+        int ip = wifiManager.getConnectionInfo().getIpAddress();
+        return String.format("%d.%d.%d.%d", (ip & 0xff), (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
+    }
 
-		Log.i(TAG, "Could not find WebService");
-		return null;
-	}
+    private OkHttpClient getClient() {
+        if (mClient != null) {
+            return mClient;
+        }
+        mClient = new OkHttpClient();
 
-	private static class CheckConnection implements Callable<String> {
-		private String _url;
-		private OkHttpClient _client;
-	
-		public CheckConnection(String url, OkHttpClient client) {
-			_url = url;
-			_client = client;
-		}
-	
-		@Override
-		public String call() {
-		    Request request = new Request.Builder()
-		    .head()
-	        .url(_url)
-	        .build();
-		    
-			Response response;
-			try {
-				response = _client.newCall(request).execute();
-			} catch (IOException e) {
-				return null;
-			}
-			
-			if(response.isSuccessful()) {
-				return _url;
-			} 
-			return null;
-		}
-	}
+        FundusAccount account = mApp.getAccount();
+        final String credentials = Credentials.basic(account.getUser(), account.getPassword());
+
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public com.squareup.okhttp.Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                Request modifiedRequest = request.newBuilder()
+                        .removeHeader("User-Agent")
+                        .addHeader("User-Agent", "Fundus-App")
+                        .addHeader("Accept", "application/json")
+                        .addHeader("Authorization", credentials)
+                        .build();
+                return chain.proceed(modifiedRequest);
+            }
+        };
+        mClient.interceptors().add(interceptor);
+
+        // TODO dispatcher
+//        Dispatcher dispatcher = new Dispatcher();
+//        dispatcher.setMaxRequests(255);
+//        mClient.setDispatcher(dispatcher);
+
+        mClient.setReadTimeout(5, TimeUnit.SECONDS);
+        mClient.setConnectTimeout(5, TimeUnit.SECONDS);
+        return mClient;
+    }
+
+    private boolean isServiceUrlValid(String url) {
+        String responseURL = new CheckConnection(url, getClient()).call();
+        return (responseURL != null);
+    }
+
+    private String discoverServiceURL() {
+        String ipAddress = getLocalIp();
+        ipAddress = ipAddress.substring(0, ipAddress.lastIndexOf("."));
+        ipAddress += ".";
+        Log.i(TAG, "Trying to find WebService with IP " + ipAddress + "<0-254>");
+
+        // executor for parallel requests
+        int range = 255;
+        int nrThreads = 255; // FIXME 255 threads maybe too much
+        ExecutorService executor = Executors.newFixedThreadPool(nrThreads);
+        CompletionService<String> complService = new ExecutorCompletionService<>(executor);
+        List<Future<String>> futures = new ArrayList<>(range);
+
+        // free memory for threads
+        System.runFinalization();
+        System.gc();
+
+        // create callables
+        for (int i = 0; i < range; i++) {
+            String curURL = "http://" + ipAddress + i + ServiceConnection.SERVICE_URI;
+            CheckConnection checkConnection = new CheckConnection(curURL, getClient());
+            futures.add(complService.submit(checkConnection));
+        }
+
+        // get results in completion order
+        for (int i = 0; i < range; i++) {
+            String possibleResult;
+            try {
+                possibleResult = complService.take().get();
+            } catch (ExecutionException e) {
+                // Ignore
+                continue;
+            } catch (InterruptedException e) {
+                // Ignore
+                continue;
+            }
+            if (possibleResult != null) {
+                // service found -> cancel other futures
+                for (Future<String> curFuture : futures) {
+                    if (!curFuture.isDone() && !curFuture.isCancelled()) {
+                        curFuture.cancel(false);
+                    }
+                }
+                String serviceUrl = possibleResult.replace(ServiceConnection.SERVICE_URI, "");
+                Log.i(TAG, "Found WebService at: " + serviceUrl);
+                return serviceUrl;
+            }
+        }
+
+        Log.i(TAG, "Could not find WebService");
+        return null;
+    }
+
+    private static class CheckConnection implements Callable<String> {
+        private String _url;
+        private OkHttpClient _client;
+
+        public CheckConnection(String url, OkHttpClient client) {
+            _url = url;
+            _client = client;
+        }
+
+        @Override
+        public String call() {
+            Request request = new Request.Builder()
+                    .head()
+                    .url(_url)
+                    .build();
+
+            Response response;
+            try {
+                response = _client.newCall(request).execute();
+            } catch (IOException e) {
+                return null;
+            }
+
+            if (response.isSuccessful()) {
+                return _url;
+            }
+            return null;
+        }
+    }
 
 }
